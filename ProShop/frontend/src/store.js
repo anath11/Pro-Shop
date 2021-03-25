5fba1056d0f6e20a5722686170b88f53826ea47e
Ashok <ashoknath@Ashoks-MBP.hub>
2021-03-25 17:52:47 +1000

    Adding payment method and shipping method to the web application

--- ProShop/frontend/src/store.js
+++ ProShop/frontend/src/store.js
@@ -9,6 +9,7 @@ import {
     userDetailsReducer,
     userUpdateProfileReducer  
 } from './reducers/userReducers.js'
+import { orderCreateReducer, orderDetailsReducer} from './reducers/orderReducers.js'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -17,7 +18,9 @@ const reducer = combineReducers({
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
-    userUpdateProfile: userUpdateProfileReducer
+    userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
