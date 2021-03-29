d7531e53306753ce5af79ba56e85aef16c144740
Ashok <ashoknath@Ashoks-MBP.hub>
2021-03-29 17:44:32 +1000

    Adding paypal api in the web app

--- ProShop/frontend/src/store.js
+++ ProShop/frontend/src/store.js
@@ -9,7 +9,12 @@ import {
     userDetailsReducer,
     userUpdateProfileReducer  
 } from './reducers/userReducers.js'
-import { orderCreateReducer, orderDetailsReducer} from './reducers/orderReducers.js'
+import { 
+    orderCreateReducer,
+    orderDetailsReducer,
+    orderPayReducer,
+    orderListMyReducer
+} from './reducers/orderReducers.js'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -20,7 +25,9 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
-    orderDetails: orderDetailsReducer
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
+    orderListMy: orderListMyReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
